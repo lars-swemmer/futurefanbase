57eed83e53632056c79d66a4fc635122830eecf7
Lars Swemmer <larsswemmer@gmail.com>
2017-05-13 17:04:15 +0200

    Add icons to custom solutions

--- resources/views/pages/custom-solutions.blade.php
+++ resources/views/pages/custom-solutions.blade.php
@@ -17,14 +17,14 @@
       <hr>
       <div class="row features">
         <div class="col-md-12 feature">
-          <img src="images/circle-icons/full-color/globe.png" alt="globe" />
+          <i style="float: left; font-size: 40px; color: #555;" class="fa fa-desktop" aria-hidden="true"></i>
           <div class="info">
             <strong>Data &amp; Website Solutions </strong>
             <p>With our in-house data and website skills we can offer custom made solutions when it comes to development. For bigger projects we can team up with other developers within our network. If you you have some specific data, product or website wishes, let us know what it is you’re looking for. </p>
           </div>
         </div>
         <div class="col-md-12 feature">
-          <img src="images/circle-icons/full-color/location.png" alt="location" />
+          <i style="float: left; font-size: 40px; color: #555;" class="fa fa-paint-brush" aria-hidden="true"></i>
           <div class="info">
             <strong>Content Creation</strong>
             <p>We have a large network consisting designers, video directors, video editors and website designers. When you’re in need of unique and specific content, we can build a special team and add real value to your online marketing campaign.</p>
@@ -33,7 +33,7 @@
       </div>
       <div class="row features">
         <div class="col-md-12 feature">
-          <img src="images/circle-icons/full-color/smartphone.png" alt="smartphone" />
+          <i style="float: left; font-size: 40px; color: #555;" class="fa fa-info-circle" aria-hidden="true"></i>
           <div class="info">
             <strong>Campaign Types </strong>
             <p>Future Fanbase uses several platforms in order to grow your fanbase and market your message. If you have other channels you want to promote, or promote your message on. Let us know so we can advise you on whether or not to add certain platform and campaign types to your marketing mix. </p>
