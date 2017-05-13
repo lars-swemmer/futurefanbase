b0d3099a2d4b8e62f3542960a5e3f925f12f5ddb
Lars Swemmer <larsswemmer@gmail.com>
2017-05-13 14:49:34 +0200

    Spotify Fans product page edited

--- resources/views/products/spotifyfans.blade.php
+++ resources/views/products/spotifyfans.blade.php
@@ -9,7 +9,7 @@
       <div class="row header">
         <div class="col-md-8 col-md-offset-2">
           <h3><i style=" color: rgb(30, 215, 96);" class="fa fa-spotify" aria-hidden="true"></i> Spotify Fans</h3>
-          <p>This unique product of Future Fanbase allows you to gather following on Spotify while collecting data of your fans. You will get a dedicated branded page on a separately hosted server. With the Connect &amp; Follow button it’s easier for people to follow and they’re only one click away to become your fan</p>
+          <p>This product of Future Fanbase allows you to gather following on Spotify while collecting data of your fans. You will get a dedicated branded page on a separately hosted server. With the Connect &amp; Follow button it’s easier for people to follow and they’re only one click away to become your next fan. XXx - This will generate data, which is always accessible in your own database. In the easy to read dashboard you will also find detailed insights and real time results. </p>
         </div>
       </div>
     </div>
@@ -38,7 +38,7 @@
     <div class="container">
       <div class="row header">
         <div class="col-md-12">
-          <h3>Subscription Plans</h3>
+          <h3>Subscription Service</h3>
           <p>Choose the subscription that fits your needs.</p>
         </div>
       </div>
@@ -86,7 +86,7 @@
                 remarketing &amp; conversion
               </div>
             </div>
-            <a class="btn-signup button-clear" href="signup.html">
+            <a class="btn-signup button-clear" href="MAILTO:hello@futurefanse.com">
               <span>Contact us</span>
             </a>
           </div>
@@ -134,7 +134,7 @@
                 remarketing &amp; conversion
               </div>
             </div>
-            <a class="btn-signup button-clear" href="signup.html">
+            <a class="btn-signup button-clear" href="MAILTO:hello@futurefanse.com">
               <span>Contact us</span>
             </a>
           </div>
@@ -181,7 +181,7 @@
                 remarketing &amp; conversion
               </div>
             </div>
-            <a class="btn-signup button-clear" href="signup.html">
+            <a class="btn-signup button-clear" href="MAILTO:hello@futurefanse.com">
               <span>Contact us</span>
             </a>
           </div>
@@ -189,7 +189,7 @@
       </div>
       <div class="row message">
         <p>
-          All of our plans come with a 1 month free when signing for a year.
+          All of our subscriptions come with a 1 month free when signing for a year.
         </p>
       </div>
       
@@ -197,17 +197,18 @@
         <div class="container">
           <div class="row header">
             <h3>Sum of benefits</h3>
+            <p>These are the reasons why you can’t miss out! </p>
           </div>
           <div class="row">
             <div class="col-md-4 feature">
               <img src="/images/circle-icons/one-color/creditcard.png" alt="creditcard" />
               <strong>Generate your own data </strong>
-              <p>Your data stays your own and can only be used to optimize your online marketing. </p>
+              <p>Your data stays your own and will only be used to serve your own benefits.</p>
             </div>
             <div class="col-md-4 feature">
               <img src="/images/circle-icons/one-color/dev.png" alt="dev" />
               <strong>Clear fan insights &amp; results  </strong>
-              <p>In the easy to read dashboard you can find clear fan insights and results of your marketing efforts. </p>
+              <p>In the easy to read dashboard you will find clear fan insights and results. </p>
             </div>
             <div class="col-md-4 feature">
               <img src="/images/circle-icons/one-color/globe.png" alt="globe" />
@@ -229,22 +230,8 @@
             <div class="col-md-4 feature">
               <img src="/images/circle-icons/one-color/locked.png" alt="locked" />
               <strong>Optimized online marketing</strong>
-              <p>With the generated data we can continually keep analyzing and optimizing your online marketing activities.</p>
+              <p>With the generated data we can continually optimize all of your online marketing activities.</p>
             </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="row" style="margin-top: 40px;">
-        <div class="col-md-12">
-          <div class="sections">
-            <div class="section active">
-              <h4>Spotify Campaign</h4>
-              <p style="font-size: 15px; color: #6C7279; line-height: 30px;">
-                Promoting this product through Future Fanbase has a big advantage. By the generated data and the implementation of the remarketing and conversion pixel we can keep optimizing and analyzing your campaign, measure real results and thus keep increasing your following on Spotify.
-              </p>
-            </div>
-            
           </div>
         </div>
       </div>
