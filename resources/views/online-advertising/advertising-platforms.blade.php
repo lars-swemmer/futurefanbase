1b35f53b45475827de830411dc4b9a6b7e266487
Lars Swemmer <larsswemmer@gmail.com>
2017-05-13 17:22:50 +0200

    Add dummy table as example

--- resources/views/online-advertising/advertising-platforms.blade.php
+++ resources/views/online-advertising/advertising-platforms.blade.php
@@ -69,32 +69,186 @@
     </div>
     <div class="container" style="margin-top: 40px;">
       <div class="clearfix"></div>
+
+
+
+
+
+
+
+
+
+
+
       <table class="table table-bordered">
         <thead>
           <tr>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Email</th>
+            <th>Future Fanbase Fee per platform type</th>
+            <th>Spotify</th>
+            <th>YouTube</th>
+            <th>Google Adwords</th>
+            <th>Facebook</th>
+            <th>Instagram</th>
+            <th>Twitter</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>John</td>
-            <td>Doe</td>
-            <td>john@example.com</td>
+            <td>Responsibility fee: Campaign budget*</td>
+            <td>5%</td>
+            <td>5%</td>
+            <td>5%</td>
+            <td>5%</td>
+            <td>5%</td>
+            <td>5%</td>
           </tr>
           <tr>
-            <td>Mary</td>
-            <td>Moe</td>
-            <td>mary@example.com</td>
+            <td>Minimum spend</td>
+            <td>High >4000</td>
+            <td>Low >1 p/d</td>
+            <td>Low >1 p/d</td>
+            <td>Low >5 p/d</td>
+            <td>Low >5 p/d</td>
+            <td>Low >1 p/d</td>
           </tr>
           <tr>
-            <td>July</td>
-            <td>Dooley</td>
-            <td>july@example.com</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><strong>Implementation per platform</strong></td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>Basic implementation fee*</td>
+            <td>€200</td>
+            <td>€150</td>
+            <td>€150</td>
+            <td>€100</td>
+            <td>€100</td>
+            <td>€100</td>
+          </tr>
+          <tr>
+            <td>Per extra advanced targeting (narrow)</td>
+            <td>x</td>
+            <td>€30</td>
+            <td>€30</td>
+            <td>€20</td>
+            <td>€20</td>
+            <td>€25</td>
+          </tr>
+          <tr>
+            <td>Per extra ad group (advanced targeting)</td>
+            <td>x</td>
+            <td>€35</td>
+            <td>€35</td>
+            <td>€25</td>
+            <td>€25</td>
+            <td>x</td>
+          </tr>
+          <tr>
+            <td>Per extra ad type</td>
+            <td>x</td>
+            <td>€35</td>
+            <td>€30</td>
+            <td>€25</td>
+            <td>€25</td>
+            <td>€20</td>
+          </tr>
+          <tr>
+            <td>Per extra month (reporting/optimizing)</td>
+            <td>x</td>
+            <td>€50</td>
+            <td>€50</td>
+            <td>€40</td>
+            <td>€40</td>
+            <td>€25</td>
+          </tr>
+          <tr>
+            <td>Implementation data*</td>
+            <td>x</td>
+            <td>€25</td>
+            <td>€25</td>
+            <td>€30</td>
+            <td>€30</td>
+            <td>€20</td>
+          </tr>
+          <tr>
+            <td>A/B Testing</td>
+            <td>x</td>
+            <td>on request</td>
+            <td>on request</td>
+            <td>on request</td>
+            <td>on request</td>
+            <td>on request</td>
+          </tr>
+          <tr>
+            <td>Conversion Measures</td>
+            <td>x</td>
+            <td>on request</td>
+            <td>on request</td>
+            <td>on request</td>
+            <td>on request</td>
+            <td>on request</td>
+          </tr>
+          <tr>
+            <td>In- exclusion specific audiences</td>
+            <td>x</td>
+            <td>on request</td>
+            <td>on request</td>
+            <td>on request</td>
+            <td>on request</td>
+            <td>on request</td>
+          </tr>
+          <tr>
+            <td>Set up: Remarketing lists/audiences</td>
+            <td>x</td>
+            <td>on request</td>
+            <td>on request</td>
+            <td>on request</td>
+            <td>on request</td>
+            <td>on request</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><strong>More options</strong></td>
+            <td><a href="mailto:hello@futurefanbase.com">Contact us</a></td>
+            <td><a href="mailto:hello@futurefanbase.com">Contact us</a></td>
+            <td><a href="mailto:hello@futurefanbase.com">Contact us</a></td>
+            <td><a href="mailto:hello@futurefanbase.com">Contact us</a></td>
+            <td><a href="mailto:hello@futurefanbase.com">Contact us</a></td>
+            <td><a href="mailto:hello@futurefanbase.com">Contact us</a></td>
           </tr>
         </tbody>
       </table>
+
+
+
+
+
+
+
+
+
+
 
       <div class="row message">
         <p>
